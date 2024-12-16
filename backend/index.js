98e0b07b5fbfef82bdc6c3e91678440b44b92a17
Imran Ali <imranranjha3464@gmail.com>
2024-12-16 16:07:16 -0700

    index.js updated

--- backend/index.js
+++ backend/index.js
@@ -23,8 +23,7 @@ const MONGODB_URI = process.env.MONGODB_URI;
 
 
 
-mongoose
-  .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(MONGODB_URI)
   .then(() => console.log("MongoDB connected successfully"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
